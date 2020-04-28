f5f55de1466c94f7278907125976361fd49d4554
hdickie <hume.dickie@live.com>
2020-04-27 21:42:35 -0700

    add database helper functions

---
+++ src/database-helper-functions.R
@@ -0,0 +1,58 @@
+#Driver.R
+source("C:/Users/HDickie/Desktop/master-root/src/config.R")
+
+#R has to be started as administrator in order for this to work. 
+#Send the job to the background. Use Get-Job to see the list. Use Receive-Job -Name <name> to get output from background jobs
+#
+start.database.command <- paste("powershell.exe ",project.src.dir,"start-database.ps1",sep="")
+
+get.database.status.command <- paste("powershell.exe ",project.src.dir,"get-database-status.ps1",sep="")
+shutdown.database.command <- paste("powershell.exe ",project.src.dir,"shutdown-database.ps1",sep="")
+
+doIHaveAdminRights <- function(){
+  suppressWarnings(
+  res <- system("net session",intern=TRUE)
+  )
+  if(length(res)==2){
+    return(TRUE)
+  }
+  return(FALSE)
+}
+
+startDatabase <- function(){
+  stopifnot(doIHaveAdminRights())
+  
+  setwd(project.src.dir)
+  return(system(start.database.command,wait=FALSE))
+}
+
+getDatabaseStatusText <- function(){
+  setwd(project.src.dir)
+  system(get.database.status.command,intern=TRUE)
+  
+  setwd(project.out.dir)
+  database.status <- readLines("database-status.txt")
+  return(database.status)
+}
+
+getDatabaseStatus <- function(){
+  return(length(getDatabaseStatusText())>=5)
+}
+
+shutdownDatabase <- function(){
+  setwd(project.src.dir)
+  return(system(shutdown.database.command,intern=TRUE))
+}
+
+submitSQL <- function(sql.string){
+  setwd(database.bin.dir)
+  sql.command <- paste("mysql -u root -pbadpassword -e \"",sql.string,"\"",sep="")
+  res <- system(sql.command,intern=TRUE)
+  
+  return(res[2:length(res)])
+}
+
+importToRawFromSandbox <- function(sandbox.table.name) {
+  query.string <- paste("CREATE TABLE ")
+  submitSQL()
+}
